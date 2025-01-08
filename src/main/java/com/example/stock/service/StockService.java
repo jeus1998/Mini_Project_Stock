039af498a085b4fc283cb2f427a1195571edc990
jeus1998 <zeus20240228@gmail.com>
2025-01-08 21:45:38 +0900

    [2025:01:08] Named Lock 활용 동시성 이슈 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,22 +3,21 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 public class StockService {
     private final StockRepository stockRepository;
     public StockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void decrease(Long id, Long quantity){
         // Stock 조회
         // 재고 감소
         // 갱신된 값을 저장
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
-
         stockRepository.saveAndFlush(stock);
     }
 }
