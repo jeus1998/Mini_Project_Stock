54e92d2b9351b752654cd7b0ce4260fd87c858e8
jeus1998 <zeus20240228@gmail.com>
2025-01-08 17:08:06 +0900

    [2025:01:08] 동시성 문제점 파악을 위한 테스트 작성

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -31,5 +35,24 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException{
+        int taskCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(taskCount);
+        for (int t = 0; t < taskCount; t++) {
+             executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }
+                finally {
+                    latch.countDown();
+                }
+             });
+        }
+        latch.await();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
     }
 }
